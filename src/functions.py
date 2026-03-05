a03a2dd11c1f6c192e2388e47c73ea45a81e4994
Eric Cheung <ericcheun251@gmail.com>
2026-03-04 21:47:20 -0800

    Add parse_payload function

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
 import torch
-
+import pandas as pd
 
 def sentiment_classifier(model, tokenizer, input: list):
     inputs = tokenizer(input, return_tensors="pt", padding=True, Truncation=True)
@@ -9,12 +9,22 @@ def sentiment_classifier(model, tokenizer, input: list):
     
     return outputs
 
-def parse_payload():
-    # parse logic
-    x = 1
+def parse_payload(input_payload: dict) -> pd.DataFrame:
+    """
+    """
+    user_id = input_payload["user_id"]
+    comments = input_payload["comments"]
+    
+    reference_table = {
+        "user_id": user_id,
+        "comments": comments
+    }
+
+    reference_dataframe = pd.DataFrame(reference_table)
+    return reference_dataframe
 
 
-def format_output(outputs, model):
+def format_output(outputs, model, reference_dataframe):
     for output in outputs.logits:
         print(output)
         predicted_class_id = torch.argmax(output, dim=-1).item()
