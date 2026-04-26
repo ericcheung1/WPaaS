9f7c2bda8d0eac6b0fef773fe93ea675db97dbe9
Eric Cheung <ericcheun251@gmail.com>
2026-04-25 23:21:30 -0700

    Change completed distilbert onnx conversion

--- conversions/distilbert_onnx.py
+++ conversions/distilbert_onnx.py
@@ -1,20 +1,24 @@
 from optimum.onnxruntime import ORTModelForSequenceClassification, ORTQuantizer
 from optimum.onnxruntime.configuration import AutoQuantizationConfig
+from optimum.onnxruntime import ORTModelForSequenceClassification, ORTOptimizer
+from optimum.onnxruntime.configuration import OptimizationConfig
 from transformers import AutoTokenizer
 
 model_id = "distilbert-base-uncased-finetuned-sst-2-english"
-save_dir = "./dstb_onnx"
+save_dir = "./distilbert_fp16_onnx"
 
 model = ORTModelForSequenceClassification.from_pretrained(model_id, export=True)
 
-quantizer = ORTQuantizer.from_pretrained(model)
+optimizer = ORTOptimizer.from_pretrained(model)
 
-qconfig = AutoQuantizationConfig.avx512_vnni(is_static=False, per_channel=True)
-
+opt_config = OptimizationConfig(
+    optimization_level=99, # Enables all fusions (LayerNorm, Attention, etc.)
+    fp16=True             
+)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-quantizer.quantize(save_dir=save_dir, quantization_config=qconfig)
+optimizer.optimize(save_dir=save_dir, optimization_config=opt_config)
 
 # Save the exported model
-# model.save_pretrained("./distilbert_onnx")
+model.save_pretrained(save_dir)
 tokenizer.save_pretrained(save_dir)
